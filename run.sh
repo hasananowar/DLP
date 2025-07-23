f17901d63473baa8854f82e1f541a121fca3f96a
hasananowar <hasan92niloy@gmail.com>
2025-07-23 14:48:04 -0500

    update

--- run.sh
+++
@@ -1,5 +0,0 @@
-python setup.py build_ext --inplace
-python gen_graph.py --data benzoin/edges1.csv
-python gen_graph.py --data benzoin/edges2.csv
-python train.py --data benzoin --epochs 300
-python train.py --data benzoin --epochs 300 --use_pair_index
